934b27441c8fcfebd68c81ec02a43697c38975dd
Ahmet ÇETİN <ahmetcetnn7@gmail.com>
2024-01-01 18:42:23 +0300

    api.php güncellendi

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ChatController;
+use App\Http\Controllers\Api\ChatRoomMessageController;
 use App\Http\Controllers\Api\SocketController;
+use App\Http\Middleware\AuthWithBearerToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +24,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth', [SocketController::class, 'auth']);
 Route::post('/register', [SocketController::class, 'register']);
+
+Route::post('messages', [ChatController::class, 'message']);
+
+Route::middleware(AuthWithBearerToken::class)->group(function () {
+    Route::resource('chats', ChatController::class);
+    Route::get('chats2/{id}', [ChatController::class, 'getChatRoomMessages']);
+    Route::resource('message', ChatRoomMessageController::class);
+    Route::get('/search/{username}', [ChatController::class, 'search']);
+    Route::resource('addChat', ChatController::class);
+});
